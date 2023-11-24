830eb214feb7bbe16725cef3b755bc040c83e632
Zark3 <101876624+Zark3@users.noreply.github.com>
2023-11-24 14:49:51 +0100

    First build
    First successful build on FRIOS2 server

---
+++ cmake-build-debug/CMakeFiles/POSSemestralka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POSSemestralka.dir/main.c.o"
+  "POSSemestralka"
+  "POSSemestralka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POSSemestralka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
