0594805e20cb1c56734ad9fb9376c22a8f92af96
marek <marek.kacina@gmail.com>
2024-01-03 22:16:10 +0100

    ships and target placement

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -98,5 +98,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/POSSemestralka.dir/DependInfo.cmake"
+  "CMakeFiles/POSBattleshipVojKac.dir/DependInfo.cmake"
   )
--- player.h
+++ player.h
@@ -1,20 +1,15 @@
-
-
-
 #ifndef POSSEMESTRALKA_PLAYER_H
 #define POSSEMESTRALKA_PLAYER_H
 
-#include "main.h"
 #include "grid.h"
 
 typedef struct player {
     short lives;
-    int myGrid[GRIDSIZE][GRIDSIZE];
+    int myGrid[10][10];
     short placedShips;
 } PLAYER;
 
 void initPlayer(PLAYER * player);
-int tryPutShipInGrid(PLAYER * player, enum ship, enum direction, short x, short y);
 int attack(PLAYER * att, PLAYER * def, short x, short y);
 
 #endif //POSSEMESTRALKA_PLAYER_H
